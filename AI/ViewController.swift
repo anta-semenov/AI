fad5b8563b4b08ab9b197bacaf0f21fc6077300c
Anton Semenov <anta.semenov@gmail.com>
2017-10-30 21:31:57 +0200

    Initial Commit

---
+++ AI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AI
+//
+//  Created by Anton on 30/10/2017.
+//  Copyright © 2017 Anton Semenov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
