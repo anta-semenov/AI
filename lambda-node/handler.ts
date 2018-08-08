d87d06c4c917157abe5e27840564a69b5aafe7da
Anton Semenov <anta.semenov@gmail.com>
2018-08-08 09:55:28 +0500

    wip

--- lambda-node/handler.ts
+++ lambda-node/handler.ts
@@ -1,14 +1,18 @@
 import AWS from 'aws-sdk'
 import { Callback } from 'aws-lambda'
 import { kohonenNet } from '../node/src/neuroNet/kohonen'
+import dateFns from 'date-fns'
 
 const symbols = ['AUD', 'EUR', 'GBP', 'CHF', 'CAD', 'JPY', 'Brent', 'Gold', 'Wheat', 'Soybean', 'XOM']
+const INPUT_DEEP = 22
+const DEFAULT_MIN = 999999999999
 
 interface Price {
   open: number
   close: number
   high: number
   low: number
+  date: string
 }
 
 enum Symbol {
@@ -26,7 +30,7 @@ enum Symbol {
 }
 
 interface InputPayload {
-  [symbol: number]: Price;
+  [symbol: string]: Price;
 }
 
 interface DayData {
@@ -47,21 +51,127 @@ interface SymbolData {
   minAbsolute: number
   maxAbsolute: number
   last20Low: number[]
-  last20max: number[]
+  last20High: number[]
   volatility: number[]
+  dayData: DayData[]
 }
 
 const getS3Data = (bucket: string, key: string): Promise<any> => {
-  const s3  =new AWS.S3()
+  const s3 = new AWS.S3()
   return s3.getObject({ Bucket: bucket, Key: key })
     .promise()
     .then((data) => JSON.parse(data.Body))
 }
 
+const putS3Data = (bucket: string, key: string, object: any): Promise<void> => {
+  const s3 = new AWS.S3()
+  return s3.putObject({
+    Bucket: bucket,
+    Key: key,
+    Body: JSON.stringify(object),
+  })
+    .promise()
+    .then(() => {})
+}
+
+const normalize = (value: number, min: number, max: number): number => (value - min) / (max - min)
+
 export async function predict(event: InputPayload, _: any, callback: Callback) {
+  const symbolsData = await getS3Data('antonsemenov-ai-files', 'symbolsData.json') as { [symbol: string]: SymbolData }
+  Object.values(Symbol).forEach((symbol: string) => {
+    const symbolData = symbolsData[symbol]
+    const input = event[symbol]
+    const inputDate = dateFns.startOfDay(dateFns.parse(input.date)).getTime()
+    if (symbolData.lastDate < inputDate) {
+      symbolData.lastDate = inputDate
+
+      symbolData.maxAbsolute = Math.max(symbolData.maxAbsolute, input.high)
+      symbolData.minAbsolute = Math.min(symbolData.minAbsolute, input.low)
+      symbolData.last20Low.unshift(input.low)
+      symbolData.last20High.unshift(input.high)
+      symbolData.volatility.unshift(Math.abs(input.high - input.low))
+
+      if (symbolData.last20Low.length > INPUT_DEEP) {
+        symbolData.last20Low.pop()
+        symbolData.last20High.pop()
+        symbolData.volatility.pop()
+      }
+
+      const dataForSet = ({
+        date: inputDate,
+        open: input.open,
+        close: input.close,
+        high: input.high,
+        low: input.low,
+        maxAbsolute: symbolData.maxAbsolute,
+        minAbsolute: symbolData.minAbsolute,
+        maxLocal: symbolData.last20High.reduce((res, item) => Math.max(res, item || 0), 0),
+        minLocal: symbolData.last20Low.reduce((res, item) => Math.min(res, item || DEFAULT_MIN), DEFAULT_MIN),
+        avgVol: Math.trunc(symbolData.volatility.reduce((res, item) => res + item, 0) * 100000 / symbolData.volatility.length) / 100000
+      })
+
+      symbolData.dayData.unshift(dataForSet)
+      if (symbolData.dayData.length > INPUT_DEEP) {
+        symbolData.dayData.pop()
+      }
+    }
+  })
+
+  await putS3Data('antonsemenov-ai-files', 'symbolsData.json', symbolsData)
+
+  // prepare kohonen output
   const kohonenAbsoluteLayers = await getS3Data('antonsemenov-ai-files', 'kohonenAbsoluteLayers.json')
   const kohonenLocalLayers = await getS3Data('antonsemenov-ai-files', 'kohonenLocalLayers.json')
-  const symbolsData = await getS3Data('antonsemenov-ai-files', 'symbolsData.json') as { [symbol: string]: SymbolData }
+  let kohonenResult: number[] = []
+
+  Object.values(Symbol).forEach((symbol: string) => {
+    const symbolData = symbolsData[symbol]
+    if (symbolData.dayData.length !== INPUT_DEEP) {
+      throw Error(`Day data size of ${symbol} not equal input size`)
+    }
+
+    const kohonenInputLocal = symbolData.dayData.map(({open, close, high, low, minLocal, maxLocal}) => {
+      return [
+        normalize(open, minLocal, maxLocal),
+        normalize(high, minLocal, maxLocal),
+        normalize(low, minLocal, maxLocal),
+        normalize(close, minLocal, maxLocal),
+      ]
+    })
+    const localTFInputs = kohonenNet(kohonenInputLocal, kohonenLocalLayers, true)
+
+    const kohonenInputAbsolute = symbolData.dayData.map(({open, close, high, low, minAbsolute, maxAbsolute}) => {
+      return [
+        normalize(open, minAbsolute, maxAbsolute),
+        normalize(high, minAbsolute, maxAbsolute),
+        normalize(low, minAbsolute, maxAbsolute),
+        normalize(close, minAbsolute, maxAbsolute),
+      ]
+    })
+    const absoluteTFInputs = kohonenNet(kohonenInputAbsolute, kohonenAbsoluteLayers, true)
+
+    kohonenResult = [...kohonenResult, ...localTFInputs, ...absoluteTFInputs]
+  })
+
+  await putS3Data('antonsemenov-ai-files', 'kohonen-result.json', kohonenResult)
+
+
+  // calling python
+  const lambda = new AWS.Lambda()
+  const params = {
+    FunctionName : 'lambda-python-dev-keras',
+    InvocationType : 'RequestResponse',
+  }
+  const pythonResponse = await lambda.invoke(params).promise()
+  if (!(pythonResponse.Payload && typeof pythonResponse.Payload === 'string')) {
+    return Promise.reject('Invalid response from our own backend')
+  }
+
+  const predictions = JSON.parse(pythonResponse.Payload) as number[]
+  const numberOfDeals = predictions.reduce((res, value) => value > 0.7 ? res + 1 : res, 0)
+
+
+
 
   const response = {
     statusCode: 200,
